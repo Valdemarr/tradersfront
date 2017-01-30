81f104db6c1b3e7097a1186fa547d49334447d40
kv19971 <kshitijverma999@hotmail.com>
2017-01-30 21:34:30 +0800

    added individual stock page

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { Router, Route, Link, hashHistory, IndexRoute  } from 'react-router';
 
-import App from './App.jsx';
+import App from './App'
+import StockInd from './pages/stockind'
 import store from './mainstore'
 
 
@@ -11,6 +12,7 @@ ReactDOM.render(
     (<Provider store={store}>
         <Router history={hashHistory}>
             <Route path="/" component={App} />
+            <Route path="/stockind/:code" component={StockInd} />
         </Router>
     </Provider>), 
     document.getElementById('app'));
