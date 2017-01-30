a168be816b44b0ba2f8cf99ab865381055064912
kv19971 <kshitijverma999@hotmail.com>
2017-01-30 21:04:02 +0800

    added router support

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { Router, Route, Link, hashHistory, IndexRoute  } from 'react-router';
 
 import App from './App.jsx';
 import store from './mainstore'
@@ -8,6 +9,8 @@ import store from './mainstore'
 
 ReactDOM.render(
     (<Provider store={store}>
-        <App />
+        <Router history={hashHistory}>
+            <Route path="/" component={App} />
+        </Router>
     </Provider>), 
     document.getElementById('app'));
